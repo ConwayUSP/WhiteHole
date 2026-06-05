9d34f383948b1992ee9f4586709772f8a43ca7b6
Jonyski <geleiaeavela@gmail.com>
2026-06-04 22:46:14 -0300

    feat: limpa o main.c e cria comando make run

---
+++ Makefile
@@ -0,0 +1,16 @@
+CC = gcc
+CFLAGS = -Wall
+TARGET = whitehole.out
+LIBS = -lraylib -lGL -lm -lpthread -ldl -lrt -lX11
+SRC = src/main.c
+
+all: game
+
+$(TARGET): $(SRC)
+	$(CC) $(CFLAGS) $(SRC) -o $(TARGET) $(LIBS)
+
+run: $(TARGET)
+	./$(TARGET)
+
+clean:
+	rm -f whitehole.out
---
+++ src/main.c
@@ -0,0 +1,45 @@
+#include "../include/raylib.h"
+
+#define MAX_BUILDINGS 100
+
+Camera2D cam = {0};
+
+int main(void) {
+  // Dimensões da tela
+  const int screenWidth = 1200;
+  const int screenHeight = 800;
+
+  InitWindow(screenWidth, screenHeight, "WhiteHole"); // Inicializando janela
+  SetTargetFPS(60); // Queremos que rode a 60 fps
+
+  // Loop de jogo
+  while (!WindowShouldClose()) // Fecha no ESC
+  {
+    //----------------------------------------------------------------------------------
+    // Update
+    //----------------------------------------------------------------------------------
+
+    //----------------------------------------------------------------------------------
+    // Renderização do jogo
+    //----------------------------------------------------------------------------------
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    BeginMode2D(cam);
+
+    EndMode2D();
+
+    //----------------------------------------------------------------------------------
+    // Renderização da UI
+    //----------------------------------------------------------------------------------
+    DrawText("WHITEHOLE", 500, 10, 40, BLACK);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
+  }
+
+  // Encerrando o programa
+  CloseWindow();
+  return 0;
+}
